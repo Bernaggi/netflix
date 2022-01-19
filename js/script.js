b3e396e4d1a58475ce8ee312753502e4d8b36783
Raggi Izar <61994369+Raggiiz@users.noreply.github.com>
2022-01-18 22:44:41 -0300

    intro

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {});
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {});
+
+
+  var netflixIntro = document.getElementById('container');
+  var content = document.querySelector('.wrapper');
+
+  setTimeout(() => {
+    netflixIntro.style.display = 'none';
+    content.style.display = 'block'
+  }, 5000)
+});
